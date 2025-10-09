2172b0387c87659556cf3ed653513a9e3da11053
fghfg <gfhdgf>
2025-10-09 15:30:06 +0300

    Обновление скриптов для vfs_emulator.py

--- test_script1.sh
+++ test_script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Тест 1: запуск с vfs-path и script"
-python3 second-stage.py --vfs-path /home/user/my_vfs --script test.vfs
+python3 vfs_emulator.py --vfs-path /home/user/my_vfs --script test.vfs
 
 echo "Тест 2: запуск только со script"
-python3 second-stage.py --script test.vfs
+python3 vfs_emulator.py --script test.vfs
 
 echo "Тест 3: запуск без параметров"
-python3 second-stage.py
+python3 vfs_emulator.py
